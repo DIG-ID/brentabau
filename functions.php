c4cf680e14399cffdcb5976b7ab4b574fd97e946
Daniel-Digid <daniel@dig.id>
2022-09-29 15:58:41 +0100

    Dev

--- functions.php
+++ functions.php
@@ -48,6 +48,24 @@ function hi_theme_footer_widgets_init() {
 add_action( 'widgets_init', 'hi_theme_footer_widgets_init' );
 
 /**
+ * Get font face styles.
+ */
+function hi_get_font_face_styles() {
+
+	return "
+	@font-face {
+			font-family: 'acumin-pro';
+			src: url('https://use.typekit.net/af/6d4bb2/00000000000000003b9acafc/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3') format('woff2'),url('https://use.typekit.net/af/6d4bb2/00000000000000003b9acafc/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3') format('woff'),url('https://use.typekit.net/af/6d4bb2/00000000000000003b9acafc/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3') format('opentype');
+			font-display: swap;
+			font-style: normal;
+			font-weight:700;
+			font-stretch: normal;
+		}		
+	";
+
+}
+
+/**
  * Enqueue styles and scripts
  */
 function hi_theme_enqueue_styles() {
---
+++ template-parts/main-header.php
@@ -0,0 +1,24 @@
+<header id="main-header" class="main-header fixed-top" itemscope itemtype="http://schema.org/WebSite">
+<nav class="navbar navbar-expand-lg navbar-dark" role="navigation" aria-label="<?php esc_attr_e( 'Main navigation', 'fc-oberwil' ); ?>">
+	<div class="custom-container">
+		<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg">
+			<span></span>
+			<span></span>
+			<span></span>
+		</button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'main',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarSupportedContent',
+						'menu_class'      => 'navbar-nav',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-nav',
+					)
+				);
+				?>
+				<?php do_action( 'socials' ); ?>
+	</div>
+</nav>
+</header>
