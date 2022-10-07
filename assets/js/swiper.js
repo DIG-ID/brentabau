c49a1e4fc0affacd8f0d1efdd305398446a4c3e6
Daniel-Digid <daniel@dig.id>
2022-10-07 12:11:22 +0100

    Dev

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,1 +1,22 @@
 import Swiper from 'swiper/bundle';
+
+$(function() {
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+});
---
+++ template-parts/single-projekte/section-intro.php
@@ -0,0 +1,55 @@
+<section class="section section-projekte__intro">
+    <div class="container">
+        <div class="row section-projektepage__row">
+            <div class="custom-container">
+                <h1 class="page__title"><span class="projekte__slash">/</span><?php the_title(); ?></h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-projekte__content">
+    <div class="custom-container">
+        <div class="row">
+            <div class="col-12">
+                <div class="projekte-single__head">
+                    <div class="container"><div class="row projekte-single__row">
+                        <div class="col-8 projekte-single__col1"><p class="projekte-single__title"><?php the_title(); ?></p></div>
+                        <div class="col-4 projekte-single__col2"><p class="projekte-single__category"><?php the_field('category') ?></p></div>
+                    </div></div>
+                </div>
+                <div class="projekte-single__wrapper">
+                    <div class="container"><div class="row">
+                    <div class="col-8 projekte-single__data1"><p class="text__dark"><?php the_field('description'); ?></p></div>
+                    <div class="col-4 projekte-single__data2"><p class="text__dark"><b>Dauer: </b><?php the_field('duration'); ?></p><p class="text__dark"><b>Arbeitskräfte: </b><?php the_field('manpower'); ?></p></div>
+                    </div></div>
+                </div>
+                <div class="projekte-single__gallery">
+                    <?php 
+                    $images = get_field('gallery');
+                    $size = 'full';
+                    if( $images ): ?>
+                    <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+                        <div class="swiper-wrapper">
+                            <?php foreach( $images as $image_id ): ?>
+                            <div class="swiper-slide"><?php echo wp_get_attachment_image( $image_id, $size ); ?>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
+                    <div thumbsSlider="" class="swiper mySwiper">
+                        <div class="swiper-wrapper">
+                            <?php foreach( $images as $image_id ): ?>
+                            <div class="swiper-slide"><?php echo wp_get_attachment_image( $image_id, $size ); ?>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
