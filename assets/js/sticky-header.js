19377fdfd96a2b79a04f027810e2dc434235e1b2
pedro-digid <pedro@dig.id>
2022-10-12 17:01:27 +0100

    up

--- assets/js/sticky-header.js
+++ assets/js/sticky-header.js
@@ -1,12 +1,22 @@
 $(function() {
-  $(window).on( 'scroll', function(){
-    let header = $('#main-header');
-    let size = header.outerHeight( true );
-    //let sticky = header.offsetTop;
-    if (this.scrollY > 150 ) {
-      header.addClass( 'sticky animate__animated animate__fadeInDown' );
-    } else {
-      header.removeClass( 'sticky animate__fadeInDown' );
-    }
+
+  let header = $('#main-header');
+  let lastScroll = 0;
+
+  $(window).on( 'scroll', function() {
+    const currentScroll = window.pageYOffset;
+    if ( currentScroll <= 0 ) {
+      //console.log('current scroll is ' + currentScroll);
+      header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+      return;
+    } 
+    if ( currentScroll > lastScroll && currentScroll > 200 && ! header.hasClass('sticky') ) {
+      //down
+      //console.log('current scroll (' + currentScroll + '), is higher then the lastscroll (' + lastScroll + ') and doenst have scroll-down class ');
+      header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+      header.addClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+    } 
+    lastScroll = currentScroll;
   });
+
 });
