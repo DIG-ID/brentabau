12f419709f14b8b9347f980c2eadd5a3fd9c730e
pedro-digid <pedro@dig.id>
2022-10-15 02:16:09 +0100

    up

--- assets/js/sticky-header.js
+++ assets/js/sticky-header.js
@@ -1,22 +1,24 @@
 $(function() {
 
-  let header = $('#main-header');
-  let lastScroll = 0;
-
-  $(window).on( 'scroll', function() {
-    const currentScroll = window.pageYOffset;
-    if ( currentScroll <= 0 ) {
-      //console.log('current scroll is ' + currentScroll);
-      header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
-      return;
-    } 
-    if ( currentScroll > lastScroll && currentScroll > 200 && ! header.hasClass('sticky') ) {
-      //down
-      //console.log('current scroll (' + currentScroll + '), is higher then the lastscroll (' + lastScroll + ') and doenst have scroll-down class ');
-      header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
-      header.addClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
-    } 
-    lastScroll = currentScroll;
-  });
+  if ( ! window.matchMedia('(max-width: 992px)').matches ) { // If media query not matches
+    let header = $('#main-header');
+    let lastScroll = 0;
+  
+    $(window).on( 'scroll', function() {
+      const currentScroll = window.pageYOffset;
+      if ( currentScroll <= 0 ) {
+        //console.log('current scroll is ' + currentScroll);
+        header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+        return;
+      } 
+      if ( currentScroll > lastScroll && currentScroll > 200 && ! header.hasClass('sticky') ) {
+        //down
+        //console.log('current scroll (' + currentScroll + '), is higher then the lastscroll (' + lastScroll + ') and doenst have scroll-down class ');
+        header.removeClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+        header.addClass( 'sticky animate__animated animate__fadeInDown animate__faster' );
+      } 
+      lastScroll = currentScroll;
+    });
+  }
 
 });
